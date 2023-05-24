0e639be68aae286e3cea35afba32cab2add76f4b
leo <leojwgulliver@gmail.com>
2023-05-24 19:36:33 +0100

    add menu loop

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,30 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    input("enter yo ball size: ");
+    loop {
+        println!("Welcome to twentyone");
+        println!("1. Play");
+        println!("2. Exit");
+        let choice: u8 = input("choose an option: ").parse::<u8>().unwrap();
+        match choice {
+            1 => game(),
+            2 => std::process::exit(000000),
+            _ => println!("please choose a valid option (1 or 2)")
+        } 
+    }
+    
 }
 
-fn input(text: &str) -> String {
+fn game() {
+
+}
+
+fn input(text: &str) -> &str {
     print!("{}", text);
     io::stdout().flush().unwrap(); //remove newline character 
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("failed to read input");
+    input = input.trim();
     return input;
 }
