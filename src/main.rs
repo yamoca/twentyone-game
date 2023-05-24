7858049e4fb73b9f0e5de7824099fec2493d4b50
leo <leojwgulliver@gmail.com>
2023-05-24 17:15:06 +0100

    implement input function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    input("enter yo ball size: ");
+}
+
+fn input(text: &str) -> String {
+    print!("{}", text);
+    io::stdout().flush().unwrap(); //remove newline character 
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read input");
+    return input;
 }
