b3948d4921f3f886a5609b40ae2fce464ab1c5a3
leo <leojwgulliver@gmail.com>
2023-06-02 11:51:19 +0100

    complete main game loop

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ use std::io::Write;
 
         TODO:
                 merge input and get_valid_input
-                clean up main "game welcome" loop 
-                    use wildcard instead of Err(_) to avoid having to check valid input twice in following match statement
+                    does it matter?`1
+                    just get rid of get valid input and place logic inside main game loop
+                
+                find way for "choice" to be immutable (fix up while loop (for loop?))
 
 */
 
@@ -18,28 +20,38 @@ fn main() {
         println!("1. Play");
         println!("2. Exit");
         let choice: u8 = match input("choose an option: ").trim().parse::<u8>() {
-            Ok(n) => n,
-            Err(_) => {
+            Ok(n) if n >= 1 && n <= 2 => n,
+            _ => {
                 println!("Invalid input, enter a valid number");
                 main(); //hacky - fix at later date
                 break;
             }
         };
 
-        match choice {
-            1 => {
-                game();
-                break;
-            }
-            2 => std::process::exit(000000),
-            _ => println!("Failed to match input to choice, enter a valid number between 1 and 2")
-        } 
+        if choice == 1 { game(); }
+        else { std::process::exit(2000) }
     }
     
 }
 
 fn game() {
-    get_valid_input();
+    let mut score: u8 = 0;
+    loop {
+        let mut choice: u8 = get_valid_input();
+        while choice >  0 {
+            choice -= 1;
+            score += 1;
+            println!("{}", score);
+
+            if score == 21 {
+                println!("you lose");
+                main(); //hacky find another way 
+            }   
+        }
+
+    }
+
+    
 }
 
 fn get_valid_input() -> u8 {
@@ -47,7 +59,7 @@ fn get_valid_input() -> u8 {
         let num = input("enter increase to the total (1-3): ");
         match num.trim().parse::<u8>() {
             Ok(n) if n >= 1 && n <= 3 => return n,
-            _ => println!("deez"),
+            _ => println!("invalid"),
         }
     }
 }
