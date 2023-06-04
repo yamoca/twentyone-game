f3cdfbe75dc5749e357ec7b2a93caf642db0caf1
leo <leojwgulliver@gmail.com>
2023-06-04 13:03:57 +0100

    refactor input system

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,6 @@ use std::io::Write;
 /* ---------------------------------------------
 
         TODO:
-                merge input and get_valid_input
-                    does it matter?`1
-                    just get rid of get valid input and place logic inside main game loop
-                
                 find way for "choice" to be immutable (fix up while loop (for loop?))
 
 */
@@ -19,25 +15,18 @@ fn main() {
         println!("Welcome to twentyone");
         println!("1. Play");
         println!("2. Exit");
-        let choice: u8 = match input("choose an option: ").trim().parse::<u8>() {
-            Ok(n) if n >= 1 && n <= 2 => n,
-            _ => {
-                println!("Invalid input, enter a valid number");
-                main(); //hacky - fix at later date
-                break;
-            }
-        };
+
+        let choice: u8 = get_valid_input("choose an option: ", 1, 2);
 
         if choice == 1 { game(); }
         else { std::process::exit(2000) }
     }
-    
 }
 
 fn game() {
     let mut score: u8 = 0;
     loop {
-        let mut choice: u8 = get_valid_input();
+        let mut choice: u8 = get_valid_input("enter increase to the total (1-3): ", 1, 3);
         while choice >  0 {
             choice -= 1;
             score += 1;
@@ -45,32 +34,25 @@ fn game() {
 
             if score == 21 {
                 println!("you lose");
-                main(); //hacky find another way 
+                main();
             }   
         }
-
     }
-
-    
 }
 
-fn get_valid_input() -> u8 {
+fn get_valid_input(text: &str, low_bound: u8, up_bound: u8) -> u8 {
     loop {
-        let num = input("enter increase to the total (1-3): ");
-        match num.trim().parse::<u8>() {
-            Ok(n) if n >= 1 && n <= 3 => return n,
-            _ => println!("invalid"),
+        print!("{}", text);
+        io::stdout().flush().unwrap(); //remove newline character 
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed to read input");
+        input = input.to_string();
+
+        //let num = input("enter increase to the total (1-3): ");
+        match input.trim().parse::<u8>() {
+            Ok(n) if n >= low_bound && n <= up_bound => return n,
+            _ => println!("Invalid input, please enter a number between {} and {}", low_bound, up_bound),
         }
     }
-}
-
-fn input(text: &str) -> String {
-    print!("{}", text);
-    io::stdout().flush().unwrap(); //remove newline character 
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read input");
-    input = input.to_string();
-
-    return input
 }
